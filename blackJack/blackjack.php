38378cee61b4c34683c150c5a8631c34614a2780
Richard Herbert <richard.herbert1980@gmail.com>
2020-03-11 15:13:04 +0000

    fix multi dimensional deck array, add ace = 1 or 21, implement multi card raw when less than or equal to 16 in hand

--- blackJack/blackjack.php
+++ blackJack/blackjack.php
@@ -1,42 +1,46 @@
 <?php
 
-function shufflePack (array $aDeck) : array {
-    //for each suit in a deck
-    foreach($aDeck as $aSuit => $suitCards){
-        //randomise the cards in a suit
-        shuffle($suitCards);
-        $aDeck[$aSuit] = $suitCards;
-    }
-
-    return $aDeck;
-}
+//function shufflePack (array $aDeck) : array {
+//    //for each suit in a deck
+//    foreach($aDeck as $aSuit => $suitCards){
+//        //randomise the cards in a suit
+////        shuffle($suitCards);
+//        $aDeck[$aSuit] = $suitCards;
+//    }
+//
+//    return $aDeck;
+//}
 
 function dealCard(array &$aDeck) {
     //pick a random suit
     $aSuit = array_rand($aDeck);
-    //pick a random card from the suit
-    $aCard = [$aSuit, array_pop($aDeck[$aSuit])];
 
-    return $aCard;
+    //pick a random card from the suit choose its name
+    $aCardKey = array_rand($aDeck[$aSuit]);
+    //find the card value using the cardName
+    $aCardValue = $aDeck[$aSuit][$aCardKey];
+
+    return ['suit' => $aSuit,'value' => $aCardValue, 'name' => $aCardKey];
 }
 
 function totalHand (array $aPlayer) : int {
     $aPlayersTotalScore = 0;
+
     //get value of all cards in hand
-    foreach ($aPlayer['cards'] as $cardInHand) {
-        if(is_int($cardInHand[1])){
-            $aPlayersTotalScore += $cardInHand[1];
-        } elseif ($cardInHand[1] == 'Ace') {
-            $aPlayersTotalScore += 11;
-        } else {
-            $aPlayersTotalScore += 10;
-        }
+    foreach ($aPlayer['cards'] as $aCardInTheHand) {
+        $aPlayersTotalScore += $aCardInTheHand['value'];
     }
+
+    //incorporate that an ace may be 1 or 11 if ace causes bust
+    if(in_array('Ace', $aPlayer['cards']) and $aPlayer['score'] > 21){
+        $aPlayersTotalScore -= 10;
+    }
+
     return $aPlayersTotalScore;
 }
 
 function checkBust(array $aPlayer) : bool {
-    return $aPlayer[2] > 21;
+    return $aPlayer['score'] > 21;
 }
 
 function makeWinningMessage (array $aPlayer) : string {
@@ -44,11 +48,13 @@ function makeWinningMessage (array $aPlayer) : string {
 
     foreach ($aPlayer['cards']as $aCard){
         if(is_array($aCard)){
-            $winningMessage = $winningMessage . ' ' . $aCard[1] . ' of ' . $aCard[0] . '<br>';
+            $winningMessage = $winningMessage . ' ' . $aCard['name'] . ' of ' . $aCard['suit'] . '<br>';
         } else {
-            $winningMessage = $winningMessage . ' ' . $aCard[1] . ' of ' . $aCard[0] . '<br>';
+            $winningMessage = $winningMessage . ' ' . $aCard['name'] . ' of ' . $aCard['suit'] . '<br>';
         }
     }
+
+    $winningMessage = $winningMessage . '<br><br>' . 'Score is: ' . $aPlayer['score'] . '<br>';
 
 
     return $winningMessage;
@@ -65,11 +71,11 @@ function andTheWinnerIs(array $aPlayer, array $anotherPlayer) : string {
 
     $winningMessage = $winningMessage . '<br><br>';
 
-    if ($aPlayer[2] > $anotherPlayer[2] and $aPlayer[2] <= 21) {
+    if ($aPlayer['score'] > $anotherPlayer['score'] and $aPlayer['score'] <= 21) {
         $winningMessage = $winningMessage . 'PLAYER WINS!!!!!';
-    } elseif ($aPlayer[2] < $anotherPlayer[2] and $anotherPlayer[2] <= 21) {
+    } elseif ($aPlayer['score'] < $anotherPlayer['score'] and $anotherPlayer['score'] <= 21) {
         $winningMessage = $winningMessage . "Sorry the house wins :-(";
-    } elseif ($aPlayer[2] == $anotherPlayer[2]) {
+    } elseif ($aPlayer['score'] == $anotherPlayer['score']) {
         $winningMessage = $winningMessage . 'It\' a draw...';
     } elseif ($aPlayer > 21){
         $winningMessage = $winningMessage . 'Sorry house wins. Player is bust.';
@@ -84,10 +90,10 @@ function andTheWinnerIs(array $aPlayer, array $anotherPlayer) : string {
 
 function playBlackjack () {
     $deck = [
-        'hearts' => [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace'],
-        'diamonds' => [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace'],
-        'clubs' => [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace'],
-        'spades' => [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace'],
+        'hearts' => [2 => 2, 3 => 3, 4 => 4, 5 =>5, 6 =>6, 7 => 7, 8 => 8, 9 => 9, 10 => 10,'Jack' => 10 ,'Queen' => 10,'King' =>10,'Ace' =>11],
+        'diamonds' => [2 => 2, 3 => 3, 4 => 4, 5 =>5, 6 =>6, 7 => 7, 8 => 8, 9 => 9, 10 => 10,'Jack' => 10 ,'Queen' => 10,'King' =>10,'Ace' =>11],
+        'clubs' => [2 => 2, 3 => 3, 4 => 4, 5 =>5, 6 =>6, 7 => 7, 8 => 8, 9 => 9, 10 => 10,'Jack' => 10 ,'Queen' => 10,'King' =>10,'Ace' =>11],
+        'spades' => [2 => 2, 3 => 3, 4 => 4, 5 =>5, 6 =>6, 7 => 7, 8 => 8, 9 => 9, 10 => 10,'Jack' => 10 ,'Queen' => 10,'King' =>10,'Ace' =>11],
     ];
 
     $player = [];
@@ -95,14 +101,13 @@ function playBlackjack () {
     $house = [];
 
     while(true) {
-        //shuffle pack
-        $deck = shufflePack($deck);
         //deal card -> player
         $player['cards'][] = dealCard($deck);
         //deal card -> player
         $player['cards'][]= dealCard($deck);
         //total player hand
-        $player[2] = totalHand($player);
+        $player['score'] = totalHand($player);
+
         //check if player busts
         if(checkBust($player)){
             echo andTheWinnerIs($player, $house);
@@ -113,12 +118,18 @@ function playBlackjack () {
         //deal card -> house
         $house['cards'][] = dealCard($deck);
         //total hand -> house
-        $house[2] = totalHand($house);
+        $house['score'] = totalHand($house);
 
         //check if bust -> house
         if (checkBust($house)){
             echo andTheWinnerIs($player ,$house);
             break;
+        }
+        while($player['score'] <= 16 and $house['score'] <= 16){
+            $player['cards'][] = dealCard($deck);
+            $house['cards'][] = dealCard($deck);
+            $player['score'] = totalHand($player);
+            $house['score'] = totalHand($house);
         }
         echo andTheWinnerIs($player, $house);
 //        //play again?
